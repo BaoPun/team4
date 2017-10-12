9ce1faad9097cde308318e6a6fdb5bb506b7c1b5
BaoPun <phungb@oregonstate.edu>
2017-10-12 06:46:17 -0700

    Comments in my section to clarify stuff

--- src/main/java/models/Game.java
+++ src/main/java/models/Game.java
@@ -62,17 +62,18 @@ public class Game {
             Method gAddCol, gTopCol, gEmptyCol;
             Game game = new Game();
             try{
+		//first 6 lines are essentially just allowing us to access private methods
                 gAddCol = Game.class.getDeclaredMethod("addCardToCol", int.class, Card.class);
                 gTopCol = Game.class.getDeclaredMethod("getTopCard", int.class);
                 gEmptyCol = Game.class.getDeclaredMethod("columnHasCards", int.class);
                 gAddCol.setAccessible(true);
                 gTopCol.setAccessible(true);
                 gEmptyCol.setAccessible(true);
-                Card top = deck.get(deck.size() - 1 - i);
-                deck.remove(deck.size() - 1 - i);
-                gAddCol.invoke(game, i, top);
-                Card t = (Card) gTopCol.invoke(game, i); //terminal
-                boolean empty = (boolean) gEmptyCol.invoke(game, i); //terminal
+                Card top = deck.get(deck.size() - 1 - i);   		//retrieve top card of deck
+                deck.remove(deck.size() - 1 - i);           		//remove it right after LuL
+                gAddCol.invoke(game, i, top);               		//call addCardToCol method
+                Card t = (Card) gTopCol.invoke(game, i); 		//terminal, call getTopCard method
+                boolean empty = (boolean) gEmptyCol.invoke(game, i); 	//terminal, call columnHasCards method
                 System.out.println("\n");
             }
             catch (NoSuchMethodException e) {
