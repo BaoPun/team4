91477c6dcde998b5b26c5eff3c250298e807d781
Scott Waddington <waddings@oregonstate.edu>
2017-10-12 15:56:50 -0700

    added remove function

--- src/main/java/models/Game.java
+++ src/main/java/models/Game.java
@@ -47,7 +47,7 @@ public class Game {
 			deck.remove(randomInt2);                            //Remove the card at the second index and copy the first card over
 			deck.add(randomInt2, tempCard);
 		}
-		
+
 		/*for(int i=0; i<52; i++){              //Print the entire deck for testing purposes
 			System.out.println(deck.get(i));
 		}*/
@@ -55,41 +55,31 @@ public class Game {
 
     public void dealFour() {
         // remove the top card from the deck and add it to a column; repeat for each of the four columns
-	for(int i = 0; i < 4; i++){
-	        //because addCardToCol is private, have to declare and invoke addCardToCol method to access the function
-            //try and catch is required for Java to not have a compiling error...
-            //terminal output will have comments, omit if you wish
-            Method gAddCol, gTopCol, gEmptyCol;
-            Game game = new Game();
-            try{
-		//first 6 lines are essentially just allowing us to access private methods
-                gAddCol = Game.class.getDeclaredMethod("addCardToCol", int.class, Card.class);
-                gTopCol = Game.class.getDeclaredMethod("getTopCard", int.class);
-                gEmptyCol = Game.class.getDeclaredMethod("columnHasCards", int.class);
-                gAddCol.setAccessible(true);
-                gTopCol.setAccessible(true);
-                gEmptyCol.setAccessible(true);
-                Card top = deck.get(deck.size() - 1);   		//retrieve top card of deck
-                deck.remove(deck.size() - 1);           		//remove it right after LuL
-                gAddCol.invoke(game, i, top);               		//call addCardToCol method
-                Card t = (Card) gTopCol.invoke(game, i); 		//terminal, call getTopCard method
-                boolean empty = (boolean) gEmptyCol.invoke(game, i); 	//terminal, call columnHasCards method
-                System.out.println("\n");
-            }
-            catch (NoSuchMethodException e) {
-                //e.printStackTrace();
-            }
-            catch (IllegalAccessException e) {
-                e.printStackTrace();
-            }
-            catch (InvocationTargetException e) {
-                //e.printStackTrace();
-            }
+	    for(int i = 0; i < 4; i++) {
+	        Card top = deck.get(deck.size() - 1);        //retrieve top card of deck
+            deck.remove(deck.size() - 1);                //remove it right after LuL
+            addCardToCol(i, top);
+            Card t = getTopCard(i);
+            boolean empty = columnHasCards(i);
+            System.out.println("\n");
         }
     }
 
     public void remove(int columnNumber) {
         // remove the top card from the indicated column
+        if (columnHasCards(columnNumber) == false)
+            return;
+        Card toRemove = getTopCard(columnNumber);
+        //System.out.println("toRemove is a " + toRemove.getValue() + " of " + toRemove.getSuit());
+        for (int i = 0; i < 4; i++) {
+            if (i != columnNumber && columnHasCards(i)) {
+                Card temp = getTopCard(i);
+                if ((toRemove.getSuit() == temp.getSuit()) && (toRemove.getValue() < temp.getValue())) {
+                    removeCardFromCol(columnNumber);
+                    return;
+                }
+            }
+        }
     }
 
     private boolean columnHasCards(int columnNumber) {
