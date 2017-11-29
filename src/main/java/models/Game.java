fd1302b9f821962cda4bc9c5da3f912645e1fa97
BaoPun <phungb@oregonstate.edu>
2017-11-28 18:36:09 -0800

    Column class added

--- src/main/java/models/Game.java
+++ src/main/java/models/Game.java
@@ -7,14 +7,17 @@ public class Game {
 
     public Deck deck = new Deck();
 
-    public java.util.List<java.util.List<Card>> cols = new ArrayList<>(4);
-
+    //public java.util.List<java.util.List<Card>> cols = new ArrayList<>(4);
+    public java.util.List<Column> cols = new ArrayList<>();
 
     public Game(){
 
-        for(int i = 0; i < 4; i++)
+        /*for(int i = 0; i < 4; i++)
         {
             cols.add(new ArrayList<Card>());
+        }*/
+        for(int i = 0; i < 4; i++) {
+            cols.add(new Column(i + 1));
         }
     }
 
@@ -49,11 +52,13 @@ public class Game {
 
     private boolean columnHasCards(int columnNumber) {
         // check indicated column for number of cards; if no cards return false, otherwise return true
-        return (this.cols.get(columnNumber).size() != 0);
+        //return (this.cols.get(columnNumber).size() != 0);
+        return (this.cols.get(columnNumber).cards.size() != 0);
     }
 
     private Card getTopCard(int columnNumber) {
-        return this.cols.get(columnNumber).get(this.cols.get(columnNumber).size()-1);
+        //return this.cols.get(columnNumber).get(this.cols.get(columnNumber).size()-1);
+        return this.cols.get(columnNumber).cards.get(this.cols.get(columnNumber).cards.size()-1);
     }
 
 
@@ -69,10 +74,14 @@ public class Game {
     }
 
     private void addCardToCol(int columnTo, Card cardToMove) {
-        cols.get(columnTo).add(cardToMove);
+
+        //cols.get(columnTo).add(cardToMove);
+        cols.get(columnTo).cards.add(cardToMove);
     }
 
     private void removeCardFromCol(int colFrom) {
-        this.cols.get(colFrom).remove(this.cols.get(colFrom).size() - 1);
+
+        //this.cols.get(colFrom).remove(this.cols.get(colFrom).size() - 1);
+        this.cols.get(colFrom).cards.remove(this.cols.get(colFrom).cards.size() - 1);
     }
 }
