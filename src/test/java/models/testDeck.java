8b3b16faf1feba5bf54c0606a091862d16050c5c
Vincent Nguyen <nguyvinc@oregonstate.edu>
2017-12-01 18:29:59 -0800

    Span Deck Test Implemented

--- src/test/java/models/testDeck.java
+++ src/test/java/models/testDeck.java
@@ -13,9 +13,6 @@ public class testDeck {
     public void testBuildDeck(){
         Deck reg = new RegDeck();
         assertEquals(52,reg.d.size());
-
-        Deck span = new SpanDeck();
-        assertEquals(50,span.d.size());
     }
 
 }
